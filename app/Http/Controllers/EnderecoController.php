bc453805d7c1fbfa9935e34b809bff91832a49d0
João Pedro Maciel <joomaciiel2@outlook.com>
2020-07-30 21:08:12 -0300

    viacep

---
+++ app/Http/Controllers/EnderecoController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class EnderecoController extends Controller
+{
+    public function index()
+    {
+        return view('busca');
+    }
+
+    public function buscar(Request $request)
+    {
+        $cep = $request->input('cep');
+
+        //viacep.com.br
+        $response = Http::get("https://viacep.com.br/ws/$cep/json")->json();
+        dd($response);
+
+    }
+}
